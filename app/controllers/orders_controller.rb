6b315bec0bdfb94ca04371af68a3f78298922738
coronnoniwa <musasi110604@gmail.com>
2023-03-27 23:44:09 +0900

    商品購入機能

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,13 +1,19 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!, only: :index
   def index
     @order_address = OrderAddress.new
     @item = Item.find(params[:item_id])
+
+    if current_user.id == @item.user_id or @item.order.present?
+      redirect_to root_path
+    end
   end
 
   def create
-    @order_address = OrderAddress.new(order_params)
     @item = Item.find(params[:item_id])
+    @order_address = OrderAddress.new(order_params)
     if @order_address.valid?
+      pay_item
       @order_address.save
       redirect_to root_path
     else
@@ -17,7 +23,16 @@ class OrdersController < ApplicationController
 
   private
   def order_params
-    params.require(:order_address).permit(:post_code, :prefecture_id, :delivery_city, :delivery_address, :delivery_building, :telephone_number, :price).merge(user_id: current_user.id)
+    params.require(:order_address).permit(:post_code, :prefecture_id, :delivery_city, :delivery_address, :delivery_building, :telephone_number).merge(user_id: current_user.id, token: params[:token], item_id: params[:item_id])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]  # 自身のPAY.JPテスト秘密鍵を記述しましょう
+    Payjp::Charge.create(
+      amount: @item.price,  # 商品の値段
+      card: order_params[:token],    # カードトークン
+      currency: 'jpy'                 # 通貨の種類（日本円）
+    )
   end
 
   # def address_params
--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,25 +1,26 @@
 class OrderAddress
   include ActiveModel::Model
-  attr_accessor :post_code, :prefecture_id, :delivery_city, :delivery_address, :delivery_building, :telephone_number, :price,
-                :user_id
+  attr_accessor :post_code, :prefecture_id, :delivery_city, :delivery_address, :delivery_building, :telephone_number,
+                :user_id, :token, :item_id
 
   with_options presence: true do
-    with_options format: { with: /\A[0-9]+\z/ } do
-      validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 },
-                        presence: { message: "can't be blank" }
-    end
+    # (with_options format: { with: /\A[0-9]+\z/ } do
+    #   validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 },
+    #                     presence: { message: "can't be blank" }
+    # end)
     validates :user_id
     validates :post_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
     validates :telephone_number, format: { with: /\A[0-9]+\z/ }
   end
   validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :token, presence: true
 
   def save
     # 寄付情報を保存し、変数donationに代入する
-    order = Order.create(user_id: user_id, item_id: item_id)
+    order = Order.create!(user_id: user_id, item_id: item_id)
     # 住所を保存する
     # donation_idには、変数donationのidと指定する
-    Address.create(post_code: post_code, prefecture_id: prefecture_id, delivery_city: delivery_city,
-                   delivery_address: delivery_address, delivery_building: delivery_building, telephone_number: telephone_number, order_id: order_id)
+    Address.create!(post_code: post_code, prefecture_id: prefecture_id, delivery_city: delivery_city,
+                   delivery_address: delivery_address, delivery_building: delivery_building, telephone_number: telephone_number, order_id: order.id)
   end
 end
